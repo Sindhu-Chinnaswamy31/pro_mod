7a384709327e81175d3bce1db6361905911b4773
Your Name <sindhucswamy@gmail.com>
2020-09-07 19:01:44 +0530

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,25 +1,29 @@
 from django.db import models
-
+from django.core import validators
+from django.core.exceptions import ValidationError
 # Create your models here.
+
 class Topic(models.Model):
-    topic_name=models.CharField(max_length=100,unique=True,blank=False)
-    
+    topic_name=models.CharField(max_length=30,unique=True,blank=False,validators=[validators.MaxLengthValidator(10)])
+
     def __str__(self):
         return self.topic_name
 
-        
+def validate_name(name):
+    if not name.isalpha():
+        raise ValidationError("{} is having charecters other than alphabets".format(name))
+
+    
 class Webpage(models.Model):
     topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100,unique=True,blank=False)
+    name=models.CharField(max_length=100,unique=True,validators=[validate_name],blank=False)
     url=models.URLField(max_length=100,unique=True,blank=False)
 
     def __str__(self):
         return self.name
-
-
 class AccessDetails(models.Model):
     webpage=models.ForeignKey(Webpage,on_delete=models.CASCADE)
-    datetime=models.DateTimeField(auto_now_add=True)
+    datetime=models.DateTimeField()
 
     def __str__(self):
-        return str(self.datetime)
+        return str(self.datetime).split(" ")[0]
